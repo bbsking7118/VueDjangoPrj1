779a8f441dd29b2f8d7851568f1907653b693d74
bbsking7118 <bbsking1@naver.com>
2023-05-01 17:41:49 +0900

    setting

--- backend/mysite/settings/product.py
+++ backend/mysite/settings/product.py
@@ -8,20 +8,27 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 
 # AWS Example
-ALLOWED_HOSTS = ['.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['43.201.145.183']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ['DATABASE_NAME'],
+#         'USER': os.environ['DATABASE_USER'],
+#         'PASSWORD': os.environ['DATABASE_PASSWORD'],
+#         'HOST': 'django-mysql.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': 'django-mysql.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-CSRF_TRUSTED_ORIGINS = ['https://*.elasticbeanstalk.com']
+CSRF_TRUSTED_ORIGINS = ['43.201.145.183']
